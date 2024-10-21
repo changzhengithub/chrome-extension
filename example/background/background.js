75fb0f1345e740f8d6fe33ce7c88b7d90f92c306
changzhengithub <1979250385@qq.com>
2024-10-21 22:15:29 +0800

    init

--- example/background/background.js
+++ example/background/background.js
@@ -1,1 +1,12 @@
-console.log('this is background!')
+// 安装
+chrome.runtime.onInstalled.addListener((details) => {
+  if(details.reason !== "install" && details.reason !== "update") return;
+  console.log('this is background!')
+
+  // 调用API请求
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "GET",
+  }).then(response => response.json())
+  .then(json => console.log(json))
+  .catch(err => console.log('Request Failed', err)); 
+});
